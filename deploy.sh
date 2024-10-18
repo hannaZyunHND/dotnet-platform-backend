c7e3785c42e31fb222f948d91984ed9442cf43eb
phamphammmmm <phamtiendat246@gmail.com>
2024-10-18 11:38:24 +0700

    commit to init pipeline

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Xóa cache build Docker
+docker builder prune -f
+
+# Xóa các image không cần thiết
+docker image prune -a -f
+
+# Xóa các container đã dừng
+docker container prune -f
+
+# Xóa các volume không cần thiết
+docker volume prune -f
+
+# Dừng các container đang chạy mà không xóa network
+docker-compose -f Web/Platform/WEBAPI/PlatformWEBAPI/docker-compose.yaml stop
+
+# Xóa các container cũ
+docker-compose -f Web/Platform/WEBAPI/PlatformWEBAPI/docker-compose.yaml rm -f
+
+# Pull image mới từ Docker Hub
+docker-compose -f Web/Platform/WEBAPI/PlatformWEBAPI/docker-compose.yaml pull
+
+# Chạy container mới
+docker-compose -f Web/Platform/WEBAPI/PlatformWEBAPI/docker-compose.yaml up -d
